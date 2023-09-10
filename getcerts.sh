2db140b42baea95a39ff6d8a41b6bf54f388be8e
robbyandrews <33102078+robbyandrews@users.noreply.github.com>
2023-09-09 21:39:23 -0700

    Added user input for the domain portions

--- getcerts.sh
+++ getcerts.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo "Enter your email: "
+read email
+echo "Enter your domain: "
+read domain
 sudo apt-get install -y certbot
-sudo certbot certonly --manual --preferred-challenges dns --email your-email -d www.your-domain.com -d your-domain.com
+sudo certbot certonly --manual --preferred-challenges dns --email $email -d www.$domain -d $domain
--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "Enter your domain: "
+read domain
 sudo mkdir -p /etc/haproxy/certs
-sudo sh -c  'cat /etc/letsencrypt/live/www.yourdomain.com/fullchain.pem /etc/letsencrypt/live/www.yourdomain.com/privkey.pem > /etc/haproxy/certs/yourdomain.com.pem'
+sudo sh -c  "cat /etc/letsencrypt/live/www.$domain/fullchain.pem /etc/letsencrypt/live/www.$domain/privkey.pem > /etc/haproxy/certs/$domain.pem"
