a52f8309d7bff98e6e212ecad96c9214b38d7424
robbyandrews <33102078+robbyandrews@users.noreply.github.com>
2023-09-09 21:22:52 -0700

    Added hashbang bash to the shell scripts.

--- getcerts.sh
+++ getcerts.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 sudo apt-get install -y certbot
 sudo certbot certonly --manual --preferred-challenges dns --email your-email -d www.your-domain.com -d your-domain.com
--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 sudo mkdir -p /etc/haproxy/certs
 sudo sh -c  'cat /etc/letsencrypt/live/www.yourdomain.com/fullchain.pem /etc/letsencrypt/live/www.yourdomain.com/privkey.pem > /etc/haproxy/certs/yourdomain.com.pem'
