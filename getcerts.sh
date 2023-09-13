d7519db8a90fa9bdef333725ac5eabd633a50096
robbyandrews <33102078+robbyandrews@users.noreply.github.com>
2023-09-13 13:39:10 -0700

    added comments and renamed config file

--- getcerts.sh
+++ getcerts.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
+# This function prompts for the domain variable to be entered
+
 add_domain (){
 echo "Domain not set, enter your domain: "
 read DOMAIN
 }
+# This function prompts for the email variable to be entered
 
 add_email (){
 echo "Email not set, enter your email"
 read EMAIL
 }
+
+# This command looks for a .env file and defines the variables contained in the .env file.
 export $(cat .env | xargs)
+
+# This statement says if the .env file is either not present or doesn't contain the DOMAIN variable prompt for it to be entered. 
+
 if [ -n "$DOMAIN" ]
 then 
 echo "Using $DOMAIN for the domain"
 else 
 add_domain
 fi
+# This statement says if the .env file is either not present or doesn't contain the EMAIL variable prompt for it to be entered. 
 
 if [ -n "$EMAIL" ]
 then 
@@ -23,6 +32,6 @@ else
 add_email
 fi
 
-
+# These commands automate the install of certbot and create the certs needed for SSL termination
 sudo apt-get install -y certbot
 sudo certbot certonly --manual --preferred-challenges dns --email $EMAIL -d www.$DOMAIN -d $DOMAIN
--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# This function prompts for the domain variable to be entered
 add_domain () {
 echo "Domain is not set in .env, please enter your domain: "
 read DOMAIN
 }
+# This command looks for a .env file and defines the variables contained in the .env file.
 export $(cat .env | xargs)
 
+# This statement says if the .env file is either not present or doesn't contain the $DOMAIN variable prompt for it to be entered. 
 if [ -n "$DOMAIN" ]
 then 
 echo "Using $DOMAIN for the domain"
@@ -15,6 +18,6 @@ fi
 
 
 
-
+# This command installs the certs from LetsEncrypt and adds them to HAProxy for use in the config file.
 sudo mkdir -p /etc/haproxy/certs
 sudo sh -c  "cat /etc/letsencrypt/live/www.$DOMAIN/fullchain.pem /etc/letsencrypt/live/www.$DOMAIN/privkey.pem > /etc/haproxy/certs/$DOMAIN.pem"
