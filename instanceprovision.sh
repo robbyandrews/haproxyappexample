dcdec109ddfed3bde84d1f4409bfaaadb97d30f5
robbyandrews <33102078+robbyandrews@users.noreply.github.com>
2023-09-14 10:16:31 -0700

    lx-dataset update for provision

--- instanceprovision.sh
+++ instanceprovision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-triton create  --name=frontend 1054c02a 5b82556d -t triton.cns.services=frontend,haproxy
-triton create  --name=backend1 1054c02a 5b82556d -t triton.cns.services=_backend._tcp:3000:priority=10,express,nodejs
-triton create  --name=backend2 1054c02a 5b82556d -t triton.cns.services=_backend._tcp:3000:priority=10,express,nodejs
+triton create  --name=frontend 433d1deb 5b82556d -t triton.cns.services=frontend,haproxy
+triton create  --name=backend1 433d1deb 5b82556d -t triton.cns.services=_backend._tcp:3000:priority=10,express,nodejs
+triton create  --name=backend2 433d1deb 5b82556d -t triton.cns.services=_backend._tcp:3000:priority=10,express,nodejs
 
 
 
